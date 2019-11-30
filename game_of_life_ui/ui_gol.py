6080c6f63fcb519a353b460cad014487faa07f62
Benjamin Noyer <bnoyer@student.le-101.fr>
2019-11-30 19:54:43 +0100

    Essai de QPainter

--- game_of_life_ui/ui_gol.py
+++ game_of_life_ui/ui_gol.py
@@ -1,10 +1,15 @@
 import sys
 import os
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtGui, uic
 
 app = QtWidgets.QApplication(sys.argv)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 window = uic.loadUi(os.path.join(dir_path, "mainwindow.ui"))
+print(type(window))
+painter = QtGui.QPainter()
+painter.begin(window)
+
+painter.drawChord(9, 9, 912, 713, 0, 0)
 window.show()
 app.exec()
